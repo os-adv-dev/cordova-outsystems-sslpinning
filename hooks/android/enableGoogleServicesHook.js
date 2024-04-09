7fb5867b4f2ed70859ab0fc1021a6a92fdabbb32
pcamilojunior <paulo.camilo.junior@outsystems.com>
2024-04-09 11:28:44 +0200

    enable google service if is false

--- hooks/android/enableGoogleServicesHook.js
+++ hooks/android/enableGoogleServicesHook.js
@@ -4,19 +4,27 @@ const path = require('path');
 module.exports = function(context) {
     const gradleConfigPath = path.join(context.opts.projectRoot, 'platforms/android/cdv-gradle-config.json');
 
-    fs.readFile(gradleConfigPath, (err, data) => {
-        if (err) throw err;
+    fs.readFile(gradleConfigPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('❌ Erro ao ler cdv-gradle-config.json:', err);
+            throw err;
+        }
 
-        const config = JSON.parse(data);
-        config.IS_GRADLE_PLUGIN_GOOGLE_SERVICES_ENABLED = true;
+        let config = JSON.parse(data);
 
-        fs.writeFile(gradleConfigPath, JSON.stringify(config, null, 2), (err) => {
-            if (err) {
-                console.log('❌ --cdv-gradle-config.json error: '+err);
-                throw err;
-            } else {
-                console.log('✅ --cdv-gradle-config.json has been updated to enable Google Services.');
-            }
-        });
+        if (config.IS_GRADLE_PLUGIN_GOOGLE_SERVICES_ENABLED === false) {
+            config.IS_GRADLE_PLUGIN_GOOGLE_SERVICES_ENABLED = true;
+
+            fs.writeFile(gradleConfigPath, JSON.stringify(config, null, 2), (err) => {
+                if (err) {
+                    console.error('❌ Erro to updat the cdv-gradle-config.json:', err);
+                    throw err;
+                } else {
+                    console.log('✅ cdv-gradle-config.json updated ok to enabled the Google Services.');
+                }
+            });
+        } else {
+            console.log('✅ Google Services is already set TRUE in cdv-gradle-config.json.');
+        }
     });
 };
